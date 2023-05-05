a15b0b6a2c5cedccd4a2cc052f4bee15377be89e
arivazhagan <arivzhagan.p@metamation.com>
2023-05-05 13:37:54 +0530

    Frequency calculation is implemented

---
+++ A.04.P01/SpellingBeeFrequency/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+internal class Program {
+   private static void Main (string[] args) {
+      Dictionary<char, int> charFrequency = new ();
+      var characters = string.Concat (File.ReadAllLines ("C:/etc/words.txt"));
+      characters.Distinct ().ToList ().ForEach (char1 => charFrequency.Add (char1, characters.ToList ().FindAll (char2 => char2 == char1).Count));
+      charFrequency = charFrequency.OrderByDescending (pair => pair.Value).ToDictionary (pair => pair.Key, pair => pair.Value);
+      int padLength = charFrequency.Values.First ().ToString ().Length;
+      foreach (var (key, value) in charFrequency)
+         Console.WriteLine ($"{key} : {value.ToString ().PadLeft (padLength)}");
+   }
+}
