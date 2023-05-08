f9665cfbdd7477ada004d671e03d2093a2d6472e
arivazhagan <arivzhagan.p@metamation.com>
2023-05-08 12:12:12 +0530

    Review 2 suggestions are implemented

--- A.04.P01/SpellingBeeFrequency/Program.cs
+++ A.04.P01/SpellingBeeFrequency/Program.cs
@@ -6,8 +6,10 @@ using System.Linq;
 internal class Program {
    private static void Main (string[] args) {
       Dictionary<char, int> charFrequency = new ();
-      var characters = string.Concat (File.ReadAllText ("C:/etc/words.txt")).ToList ();
-      characters.Distinct ().Where (c => c >= 'A' && c <= 'Z').ToList ().ForEach (char1 => charFrequency.Add (char1, characters.FindAll (char2 => char2 == char1).Count));
+      foreach (var character in File.ReadAllText ("C:/etc/words.txt")) {
+         if (character >= 'A' && character <= 'Z')
+            charFrequency[character] = charFrequency.ContainsKey (character) ? charFrequency[character] += 1 : 0;
+      }
       charFrequency = charFrequency.OrderByDescending (pair => pair.Value).ToDictionary (pair => pair.Key, pair => pair.Value);
       int padLength = charFrequency.Values.First ().ToString ().Length;
       foreach (var (key, value) in charFrequency.Take (7))
