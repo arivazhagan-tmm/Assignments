8d7a9bd1b9f913094144251888c3e9331fe99bc2
arivazhagan <arivzhagan.p@metamation.com>
2023-05-08 11:11:13 +0530

    Review suggestions are implemented

--- A.04.P01/SpellingBeeFrequency/Program.cs
+++ A.04.P01/SpellingBeeFrequency/Program.cs
@@ -6,11 +6,11 @@ using System.Linq;
 internal class Program {
    private static void Main (string[] args) {
       Dictionary<char, int> charFrequency = new ();
-      var characters = string.Concat (File.ReadAllLines ("C:/etc/words.txt"));
-      characters.Distinct ().ToList ().ForEach (char1 => charFrequency.Add (char1, characters.ToList ().FindAll (char2 => char2 == char1).Count));
+      var characters = string.Concat (File.ReadAllText ("C:/etc/words.txt")).ToList ();
+      characters.Distinct ().Where (c => c >= 'A' && c <= 'Z').ToList ().ForEach (char1 => charFrequency.Add (char1, characters.FindAll (char2 => char2 == char1).Count));
       charFrequency = charFrequency.OrderByDescending (pair => pair.Value).ToDictionary (pair => pair.Key, pair => pair.Value);
       int padLength = charFrequency.Values.First ().ToString ().Length;
-      foreach (var (key, value) in charFrequency)
-         Console.WriteLine ($"{key} : {value.ToString ().PadLeft (padLength)}");
+      foreach (var (key, value) in charFrequency.Take (7))
+         Console.WriteLine ($"{key,2} {value.ToString ().PadLeft (padLength)}");
    }
 }
