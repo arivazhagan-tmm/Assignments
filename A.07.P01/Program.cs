8d992712d8adec6a514160f338deb6cfe130efc4
arivazhagan <arivzhagan.p@metamation.com>
2023-06-06 17:18:29 +0530

    Review 3 changes

--- A.07.P01/Program.cs
+++ A.07.P01/Program.cs
@@ -1,7 +1,7 @@
 ﻿internal class Program {
    private static void Main (string[] args) {
-      string[] testCases = new string[] {"12345", "0.000325", "10.54E2", "-1.5E-2", "1.5E2.5", "1.E3", "1.E+3", "12.54e3.", "12.", "12.e1", ".325", " +625 ",
-                                         "6.25e0", "6.0e0", "+6.25E1", "*6.25", "1.625", "15a1", "1.567*2", "+-12", "12.-5", ".e1", "-0.325", "  12.456    " };
+      string[] testCases = new string[] {"-1.5E-2", "12345", "0.000325", "10.54E2", "1.5E2.5", "1.E3", "1.E+3", "12.54e3.", "12.", "12.e1", ".325", " +625 ",
+                                         "6.25e0", "6.0e0", "6.25e-1", "+6.25E1", "*6.25", "1.625", "15a1", "1.567*2", "+-12", "12.-5", ".e1", "-0.325", "  12.456    " };
       foreach (var s in testCases) {
          double.TryParse (s, out double value);
          Console.WriteLine ($"\nText          : \"{s}\"\nCustom Parse  : {Parse (s)}\ndouble.Parse  : {value}\n------------\n");
@@ -15,7 +15,7 @@
       s = sign is '-' or '+' ? s[1..] : s;
       double value = 0.0;
       bool validDecimal = false;
-      int expnt = 0; //Exponent
+      int expnt = 0; // Exponent
       for (int i = 0; i < s.Length; i++) {
          char c = s[i];
          switch (c) {
@@ -24,12 +24,16 @@
                if (!validDecimal) return double.NaN;
                break;
             case 'e' or 'E':
-               value *= int.TryParse (s[(i + 1)..].ToString (), out expnt) ? Math.Pow (10, expnt) : double.NaN;
+               var tmp = Parse (s[(i + 1)..]);
+               if (tmp == (int)tmp) {
+                  for (int j = 0; j < Math.Abs (tmp); j++)
+                     value *= tmp < 0 ? 0.1 : 10;
+               } else
+                  value = double.NaN;
                i = s.Length;
                break;
             case >= '0' and <= '9':
-               var tmp = int.Parse (c.ToString ());
-               value = validDecimal ? value + tmp * Math.Pow (10, --expnt) : value * 10 + tmp;
+               value = validDecimal ? value + (c - 48) * Math.Pow (10, --expnt) : value * 10 + c - 48;
                break;
             default: return double.NaN;
          }
