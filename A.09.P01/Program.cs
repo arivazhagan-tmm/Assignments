c2799d4ef2f05d3bb6ee869fc8a4ceb45872f097
arivazhagan <arivzhagan.p@metamation.com>
2023-06-14 14:32:34 +0530

    Implemented TQueue

---
+++ A.09.P01/Program.cs
@@ -0,0 +1,56 @@
+﻿using TStack;
+
+public class Program {
+   public static void Main (string[] args) {
+      TQueue<int> q = new ();
+      int n;
+      for (n = 1; n < 5; n++)
+         Enqueue (q, n);
+      Console.WriteLine ();
+      for (n = 0; n < 3; n++)
+         Dequeue (q);
+      Console.WriteLine ();
+      for (n = 1; n <5; n++)
+         Enqueue (q, n * 100);
+      Console.WriteLine ($"\nCurrent queue size is : {q.Length}\n");
+      for (n = 1; n <4; n++)
+         Dequeue (q);
+      Console.WriteLine ($"\nIs Queue empty : {q.IsEmpty}\n");
+      for (n = 1; n < 4; n++)
+         Enqueue (q, n * 10); 
+      Console.WriteLine ($"\nIs Queue empty : {q.IsEmpty}\n");
+      while (!q.IsEmpty)
+         Dequeue (q);
+      Console.WriteLine ($"\nIs Queue empty : {q.IsEmpty}\n");
+      for (n = 1; n <= 4; n++)
+         Enqueue (q, n * 10);
+      Console.WriteLine ();
+      Dequeue (q);
+      Console.WriteLine ($"\nCurrent queue size is : {q.Length}");
+
+      //Console.WriteLine (q.IsEmpty);
+      //for (int n = 0; n < 4; n++) {
+      //   q.Enqueue (n);
+      //}
+      //q.Dequeue ();
+      //q.Dequeue ();
+      //Console.WriteLine (q.IsEmpty);
+      //for (int n = 0; n < 2; n++) {
+      //   q.Enqueue (n * 10);
+      //}
+      //Console.WriteLine (q.IsEmpty);
+      //for (int n = 0; n < q.Length; n++) {
+      //   Console.WriteLine (q.Dequeue ());
+      //}
+      //Console.WriteLine (q.IsEmpty);
+      //for (int n = 0; n < 4; n++) {
+      //   q.Enqueue (n * 10);
+      //}
+   }
+
+   static void Dequeue (TQueue<int> q) => Console.Write ($"Dequeued : {q.Dequeue ()}\n");
+   static void Enqueue (TQueue<int> q, int n) {
+      q.Enqueue (n);
+      Console.Write ($"Enqueued : {n}\n");
+   }
+}
---
+++ A.09.P01/TQueue.cs
@@ -0,0 +1,32 @@
+﻿
+namespace TStack {
+   internal class TQueue<T> {
+      T[] mData = new T[4];
+      int mUsed, mRead, mWrite;
+      public bool IsEmpty => mUsed == 0;
+      public int Length => mData.Length;
+      public void Enqueue (T t) {
+         if (mUsed == Length) {
+            var tmp = new T[2 * mUsed];
+            for (int i = 0; i < mUsed; i++) {
+               tmp[i] = mData[mRead % Length];
+               mRead++;
+            }
+            mData = tmp;
+            mWrite = mUsed;
+            mRead = 0;
+         }
+         if (mWrite == Length && mUsed < Length)
+            mWrite = 0;
+         mData[mWrite++] = t;
+         mUsed++;
+      }
+      public T Dequeue () {
+         if (IsEmpty) throw new Exception ("Queue Empty");
+         T t = mData[mRead % Length];
+         mRead++;
+         mUsed--;
+         return t;
+      }
+   }
+}
