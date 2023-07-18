59d08b25d4b7fc053b4763d6d4e2d07a1383c5b5
arivazhagan <arivzhagan.p@metamation.com>
2023-07-18 18:06:28 +0530

    Implemented double ended queue

---
+++ A.10.P02/Program.cs
@@ -0,0 +1,28 @@
+﻿using A._10.P02;
+internal class Program {
+   private static void Main (string[] args) {
+      TDeque<int> q = new ();
+      Random r = new ();
+      for (int i = 1; i <= 100; i++) {
+         var tmp = r.NextDouble ();
+         if (tmp <= 0.25) {
+            q.HeadEnqueue (i);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine ($"HE : {i}");
+         } else if (tmp is <= 0.5 and >= 0.25) {
+            q.TailEnqueue (i);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine ($"TE : {i}");
+         } else if (tmp is <= 0.75 and >= 0.5 && !q.IsEmpty) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine ($"HD : {q.HeadDequeue ()}");
+         } else if (!q.IsEmpty) {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine ($"TD : {q.TailDequeue ()}");
+         }
+         Console.ResetColor ();
+         if (q.IsEmpty)
+            Console.WriteLine ("Queue Empty!");
+      }
+   }
+}
---
+++ A.10.P02/TDeque.cs
@@ -0,0 +1,39 @@
+﻿namespace A._10.P02 {
+   public class TDeque<T> {
+      T[] mData = new T[4];
+      int mUsed;
+      public bool IsEmpty => mUsed == 0;
+      public int Length => mData.Length;
+      public void HeadEnqueue (T t) {
+         if (mUsed == Length)
+            UpdateQueue (2 * mUsed);
+         else if (!IsEmpty)
+            UpdateQueue (Length);
+         mData[0] = t;
+         mUsed++;
+      }
+      public T HeadDequeue () {
+         if (IsEmpty) throw new Exception ("Queue Empty");
+         T t = mData[0];
+         mUsed--;
+         UpdateQueue (Length, 0, 1);
+         return t;
+      }
+      public void TailEnqueue (T t) {
+         if (mUsed == Length)
+            UpdateQueue (2 * mUsed, 0, 0);
+         mData[mUsed++] = t;
+      }
+      public T TailDequeue () {
+         if (IsEmpty) throw new Exception ("Queue Empty");
+         return mData[--mUsed];
+      }
+      /// <summary> Updates queue to the given size where x and y are set depends on enqueue and dequeue</summary>
+      void UpdateQueue (int size, int x=1, int y=0) {
+         var tmp = new T[size];
+         for (int i = 0; i < mUsed; i++)
+            tmp[i + x] = mData[i + y];
+         mData = tmp;
+      }
+   }
+}
