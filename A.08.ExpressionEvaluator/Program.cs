2b547dff08ffd52b3fcf36ad8aa6bd7dd45af970
arivazhagan <arivzhagan.p@metamation.com>
2023-08-21 15:40:49 +0530

    Implemented Number reversing

--- A.08.ExpressionEvaluator/Program.cs
+++ A.08.ExpressionEvaluator/Program.cs
@@ -2,19 +2,17 @@
 
 class Program {
    static void Main (string[] args) {
-      var eval = new Evaluator ();
-      for (; ; ) {
-         Console.Write ("> ");
-         string text = Console.ReadLine () ?? "";
-         if (text == "exit") break;
-         try {
-            double result = eval.Evaluate (text);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine (result);
-         } catch (Exception e) {
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine (e.Message);
+      var rand = new Random ();
+      Console.WriteLine ($"Number\t: Reversed\n");
+      for (int i = 0; i < 100; i++) {
+         int n = rand.Next (100, 99999), result = 0, tmp = n;
+         while (tmp > 0) {
+            result = result * 10 + (tmp % 10);
+            tmp /= 10;
          }
+         var output = result == n ? $"{n}\t: {result}\t\t: Palindrome" : $"{n}\t: {result}";
+         Console.ForegroundColor = result == n ? ConsoleColor.Green : Console.ForegroundColor; 
+         Console.WriteLine (output);
          Console.ResetColor ();
       }
    }
