0dbeb5864beff85127730afb184cd928063b96f7
arivazhagan <arivzhagan.p@metamation.com>
2023-08-29 15:48:59 +0530

    Implemented Multiplication Table

---
+++ T08/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+internal class Program {
+   static void Main () {
+      GetResponse ("Enter the number\t: ", out int a);
+      GetResponse ("Enter the length\t: ", out int b);
+      var (c, d) = (1, b.ToString ().Length + 1);
+      WriteLine ();
+      while (c <= b) WriteLine ($"{a} x" + $"{c}".PadLeft (d) + $" = {a * c++}");
+   }
+
+   static bool GetResponse (string prompt, out int n) {
+      Write (prompt); return int.TryParse (ReadLine (), out n);
+   }
+}
