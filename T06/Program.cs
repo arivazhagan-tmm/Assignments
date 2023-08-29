423feaf33327020cab255a37211458e5333c720e
arivazhagan <arivzhagan.p@metamation.com>
2023-08-29 15:36:19 +0530

    Implemented Palindrome Check

---
+++ T06/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program {
+   private static void Main () {
+      while (true) {
+         Console.Write ("Type the word : ");
+         var word = Console.ReadLine ();
+         if (word?.Length >= 3) {
+            var (i, len) = (0, word.Length);
+            bool isPalindrome = true;
+            for (; i < len; i++) {
+               if (word[i] != word[^(i + 1)]) {
+                  isPalindrome = false;
+                  break;
+               }
+            }
+            Console.WriteLine (isPalindrome ? $"{word}\t: Palindrome\n" : $"{word}\t : Not a palindrome\n");
+         } else
+            break;
+      }
+   }
+}
