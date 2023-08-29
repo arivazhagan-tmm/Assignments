0f1f94243984f6e9e91f41a7732ff8dc186a744b
arivazhagan <arivzhagan.p@metamation.com>
2023-08-29 12:51:22 +0530

    Implemented Fibonacci Series

---
+++ T03/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program {
+   static void Main (string[] args) {
+      Console.Write ("Enter the number of sequence : ");
+      if (int.TryParse (Console.ReadLine (), out int n)) {
+         var (a, b, c) = (0, 1, 0);
+         Console.Write ($"{a}, {b}");
+         for (int i = 2; i < n; i++) {
+            c = a + b;
+            Console.Write ($", {c}");
+            (a, b) = (b, c);
+         }
+         Console.WriteLine ();
+      } else { Console.WriteLine ("Invalid Input"); }
+   }
+}
