509ee615de2c7cca5eb6775bbc2149d5b4156cf6
arivazhagan <arivzhagan.p@metamation.com>
2023-08-30 10:19:14 +0530

    Implemented Isogram

---
+++ T15/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+internal class Program {
+   static void Main () {
+      while (true) {
+         Write ("Type the word\t: ");
+         var word = ReadLine ();
+         if (!string.IsNullOrEmpty (word)) Write (Isogram (word) ? "\t\tIsogram\n" : "\t\tNot Isogram\n");
+         else break;
+      }
+   }
+   static bool Isogram (string word) => word.Distinct ().Count () == word.Length;
+}
