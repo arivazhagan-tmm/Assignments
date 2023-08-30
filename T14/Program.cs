3b1c0c845a1a8bd3c3d4ddc2274100d0b79ff6f0
arivazhagan <arivzhagan.p@metamation.com>
2023-08-30 10:09:19 +0530

    Implemented Reduced String

---
+++ T14/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using static System.Console;
+internal class Program {
+   static void Main () {
+      while (true) {
+         Write ("Type the sequence : ");
+         var str = ReadLine ();
+         if (!string.IsNullOrEmpty (str) && str.Length > 3) {
+            var sb = new StringBuilder ();
+            for (int i = 0, len = str.Length; i < len;) {
+               if (i == len - 1) sb.Append (str[i++]);
+               else if (str[i] == str[i + 1]) i += 2;
+               else sb.Append (str[i++]);
+            }
+            WriteLine ($"\nSequence is reduced to\t : {sb}\n");
+         }
+      }
+   }
+}
