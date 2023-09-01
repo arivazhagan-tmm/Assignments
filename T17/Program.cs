5c3a758082ee2f8e10e43f7e8f8c2d73246197f4
arivazhagan <arivzhagan.p@metamation.com>
2023-09-01 18:10:00 +0530

    Implemented String Permutation

---
+++ T17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using static System.Console;
+
+class Program {
+   private static void Main (string[] args) {
+      while (true) {
+         Write ("\nType the 3 Letter word : ");
+         var word = ReadLine ();
+         if (!string.IsNullOrEmpty (word)) {
+            WriteLine ($"\nPermutaions:");
+            var len = word.Length;
+            var padding = 15;
+            var sb = new StringBuilder ();
+            for (int i = 0; i < len; i++) {
+               for (int j = i; j - i < len; j++) sb.Append (word[j % len]);
+               WriteLine ($"{sb}".PadLeft (padding));
+               WriteLine ($"{new string (sb.ToString ().Reverse ().ToArray ())}".PadLeft (padding));
+               sb.Clear ();
+            }
+         } else break;
+      }
+   }
+}
