6865c1659a1556d04c46f11c57814de079eb7a0b
arivazhagan <arivzhagan.p@metamation.com>
2023-09-01 18:13:52 +0530

    Implemented Armstrong Number

---
+++ T18/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+class Program {
+   static void Main (string[] args) {
+      while (true) {
+         Write ("Enter the number\t: ");
+         if (int.TryParse (ReadLine (), out int n) && n > 0) {
+            var (str, result) = (n.ToString (), 0.0);
+            var len = str.Length;
+            foreach (var ch in str) result += Math.Pow (double.Parse (ch.ToString ()), len);
+            WriteLine (result == n ? "Armstrong" : "Not Armstrong");
+         } else break;
+      }
+   }
+}
