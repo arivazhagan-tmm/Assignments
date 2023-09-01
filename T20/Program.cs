0de184fd01efcbcd8e07629d9002e68c5a67ccda
arivazhagan <arivzhagan.p@metamation.com>
2023-09-01 18:32:13 +0530

    Implemented Individual Digit Printing

---
+++ T20/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+class Program {
+   static void Main () {
+      while (true) {
+         Write ("\nEnter the Number\t: ");
+         var txt = ReadLine ();
+         if (double.TryParse (txt, out double f)) {
+            if (txt.Contains ('.')) {
+               var split = txt.Split ('.');
+               var (integral, factorial) = (split[0].Replace (".", ""), split[1]);
+               Print ("\nIntegral Part : ", integral);
+               Print ("\nFactorial Part : ", factorial);
+            } else Print ("\nIntegral Part : ", txt);
+         } else break;
+      }
+   }
+   static void Print (string message, string str) {
+      WriteLine (message);
+      foreach (char i in str) Write ($"{i} ");
+   }
+}
