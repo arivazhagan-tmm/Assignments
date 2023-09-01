130392b5eea7a0e6b0cda8b41c50498917e3c3d0
arivazhagan <arivzhagan.p@metamation.com>
2023-09-01 18:18:36 +0530

    Implemented Factorial

---
+++ T19/Program.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+class Program {
+   static void Main (string[] args) {
+      while (true) {
+         Write ("Enter the number : ");
+         if (int.TryParse (ReadLine (), out int n)) {
+            long fact = 1;
+            do fact *= n * --n; while ((--n - 1) > 0);
+            Write ($"Factorial : {fact}\n");
+         } else break;
+      }
+   }
+}
