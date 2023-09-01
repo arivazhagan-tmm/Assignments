26cc68248ac9eecc64ccbaf1cbd776eb106a8b3b
arivazhagan <arivzhagan.p@metamation.com>
2023-09-01 18:07:54 +0530

    Implemented Abecedarian word checking

---
+++ T16/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+class Program {
+   static void Main () {
+      var words = File.ReadAllLines (@"C:\etc\words.txt");
+      var result = words[0];
+      foreach (var word in words) {
+         if (IsAbecedarian (word)) {
+            result = word.Length > result.Length ? word : result;
+            WriteLine (word);
+         }
+      }
+      WriteLine ($"Longest abecedarian word\t: {result}");
+   }
+
+   static bool IsAbecedarian (string str) {
+      for (int i = 0; i < str.Length - 1; i++)
+         if (str[i] > str[i + 1]) return false;
+      return true;
+   }
+}
