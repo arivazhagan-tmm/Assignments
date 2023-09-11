231d66310cb51b002c4fd08076b7d2f3ac0d68fd
arivazhagan <arivzhagan.p@metamation.com>
2023-09-11 14:56:49 +0530

    Implemented Chess Board

---
+++ T07/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using static System.Console;
+
+OutputEncoding = Encoding.Unicode;
+WriteLine ("┌───┬───┬───┬───┬───┬───┬───┬───┐");
+var blackCoins = " ♜ │ ♞ │ ♝ │ ♛ │ ♚ │ ♝ │ ♞ │ ♜ │";
+var blackPawns = " ♟ │ ♟ │ ♟ │ ♟ │ ♟ │ ♟ │ ♟ │ ♟ │";
+var whiteCoins = " ♖ │ ♘ │ ♗ │ ♕ │ ♔ │ ♗ │ ♘ │ ♖ │";
+var whitePawns = " ♙ │ ♙ │ ♙ │ ♙ │ ♙ │ ♙ │ ♙ │ ♙ │";
+var connector = "\n├───┼───┼───┼───┼───┼───┼───┼───┤";
+var verticalBar = "│";
+for (int i = 0, size = 8; i < size; i++) {
+   Write (verticalBar);
+   if (i is 0) Write (blackCoins);
+   else if (i is 1) Write (blackPawns);
+   else if (i == size - 2) Write (whitePawns);
+   else if (i == size - 1) Write (whiteCoins);
+   else
+      for (int j = 0; j < size; j++) Write (verticalBar.PadLeft (4));
+   if (i == size - 1) break;
+   WriteLine (connector);
+}
+WriteLine ("\n└───┴───┴───┴───┴───┴───┴───┴───┘\n\n");
