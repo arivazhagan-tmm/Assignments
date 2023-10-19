4f6e71e9ae964e9b9f5dc473b6a9aabbb01d8401
Arivazhagan P <arivazhagan.p@metamation.com>
2023-10-19 15:18:44 +0530

    Review 2 changes

--- T15/Program.cs
+++ T15/Program.cs
@@ -1,24 +1,22 @@
 ﻿using static System.Console;
 
 internal class Program {
+
+   // Gets an input string from the user and checks if it is an 'Isogram'.
    static void Main () {
-      // While loop runs till the user enters a valid word or wishes to continue.
       while (true) {
          Write ("Enter a valid word : ");
          var word = ReadLine () ?? "";
-         // word is valid when it is not empty and formed only with alphabets.
          bool isValid = !string.IsNullOrEmpty (word) && word.All (char.IsLetter);
          if (isValid) {
             Write (IsIsogram (word) ? "\t\t\tIsogram" : "\t\t\tNot Isogram");
             Write ("\nDo you want to continue? (Y/N) : ");
-            // Terminating the program if user's input is other than y or Y.
             if (ReadLine ()?.ToLower () is not "y") break;
             WriteLine ();
          }
-         // If the word is not valid, user is prompted to enter the valid word.
       }
    }
 
-   // The word is termed as ISOGRAM if it doesn't contain any duplicate alphabet.
+   // Checks if the given string is an 'Isogram' or not. Isogram is a word that doesn't contain any duplicate letters.
    static bool IsIsogram (string word) => word.Distinct ().Count () == word.Length;
 }
