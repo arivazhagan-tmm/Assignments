9d708b3392f72142b9ff63eebd09653d6b01a8fd
Arivazhagan P <arivazhagan.p@metamation.com>
2023-10-25 12:17:23 +0530

    T21 : Swap numbers

---
+++ T21/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Console;
+
+internal class Program {
+
+   // Performs the swapping for the user's input.
+   private static void Main (string[] args) {
+      GetResponse ("Enter the first number : ", out int a);
+      GetResponse ("Enter the second number : ", out int b);
+      Swap (ref a, ref b);
+      WriteLine ();
+      WriteLine ($"After swapping, first number is {a} and second number is {b}.");
+   }
+
+   // Swaps given two numbers and stores in their actual address.
+   static void Swap (ref int a, ref int b) => (a, b) = (b, a);
+
+   // Getting the numeric response from the user for the given prompt.
+   static void GetResponse (string prompt, out int a) {
+      Write (prompt);
+      while (!int.TryParse (ReadLine (), out a)) {
+         ForegroundColor = ConsoleColor.Red;
+         WriteLine ("\tPlease enter any numeric value!");
+         ResetColor ();
+         Write (prompt);
+      }
+   }
+}
