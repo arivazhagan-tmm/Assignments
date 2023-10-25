4768ae516a38e10550a56160d1db7b41b1d6575d
Arivazhagan P <arivazhagan.p@metamation.com>
2023-10-25 17:31:31 +0530

    T22 : Review fix for removing unused parameters, colon space, index handling.

--- T22/Program.cs
+++ T22/Program.cs
@@ -3,7 +3,7 @@
 internal class Program {
 
    // Swaps digits of a number based on user given index values.
-   private static void Main (string[] args) {
+   private static void Main () {
       var rand = new Random ();
       while (true) {
          var num = rand.Next (1000, 100000).ToString ();
@@ -11,16 +11,15 @@ internal class Program {
          WriteLine ($"The number is {num}.");
          GetResponse ("Enter the first index\t: ", len, out int i);
          GetResponse ("Enter the second index\t: ", len, out int j);
-         var (m, n) = (num[j], num[i]);
-         Write ($"Result : ");
+         Write ($"Result:");
          for (int k = 0; k < len; k++) {
-            if (k == i) Print (m);
-            else if (k == j) Print (n);
+            if (k == i) Print (num[j]);
+            else if (k == j) Print (num[i]);
             else Write ($" {num[k]}");
          }
          WriteLine ();
          WriteLine ();
-         Write ("Do you want to continue? (Y/N) : ");
+         Write ("Do you want to continue? (Y/N): ");
          if (ReadLine ()?.ToLower () is not "y") break;
          WriteLine ();
       }
@@ -29,9 +28,11 @@ internal class Program {
    // Getting a valid numeric value from the user for the given prompt.
    static void GetResponse (string prompt, int max, out int i) {
       Write (prompt);
-      while (!int.TryParse (ReadLine (), out i) || i >= max) {
+      while (!int.TryParse (ReadLine (), out i) || i >= max || i < 0) {
          ForegroundColor = ConsoleColor.Red;
-         var message = i >= max ? $"\tIndex should be less than {max}" : "\tPlease enter valid numeric value!";
+         string message1 = $"\tPlease choose the index between 0 and {max - 1}.",
+                message2 = "\tPlease enter valid numeric value.";
+         var message = i >= max || i < 0 ? message1 : message2;
          WriteLine (message);
          ResetColor ();
          Write (prompt);
@@ -41,7 +42,7 @@ internal class Program {
    // Prints the given character in green color.
    static void Print (char c) {
       ForegroundColor = ConsoleColor.Green;
-      Write (" " + c);
+      Write ($" {c}");
       ResetColor ();
    }
 }
