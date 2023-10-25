72e47fd19d2cbe1eec86cc2c5bfe920fa61984e8
Arivazhagan P <arivazhagan.p@metamation.com>
2023-10-25 14:04:38 +0530

    Added methods and minor changes

--- T20/Program.cs
+++ T20/Program.cs
@@ -1,21 +1,41 @@
 ﻿using static System.Console;
+
 class Program {
+
+   // Prints the integral & factorial part of the decimal number given by the user.
    static void Main () {
       while (true) {
-         Write ("\nEnter the Number\t: ");
-         var txt = ReadLine ();
-         if (double.TryParse (txt, out double f)) {
-            if (txt.Contains ('.')) {
-               var split = txt.Split ('.');
-               var (integral, factorial) = (split[0].Replace (".", ""), split[1]);
-               Print ("\nIntegral Part : ", integral);
-               Print ("\nFactorial Part : ", factorial);
-            } else Print ("\nIntegral Part : ", txt);
-         } else break;
+         GetResponse ("Enter a decimal number : ", out double f);
+         var txt = f.ToString ();
+         if (txt.Contains ('.')) {
+            var split = txt.Split ('.');
+            var (integral, factorial) = (split[0].Replace (".", ""), split[1]);
+            Print ("Integral Part\t: ", integral);
+            Print ("Factorial Part\t: ", factorial);
+         } else Print ("Integral Part\t: ", txt);
+         WriteLine ();
+         WriteLine ();
+         Write ("Do you want to continue? (Y/N) : ");
+         if (ReadLine ()?.ToLower () is not "y") break;
+         WriteLine ();
       }
    }
+
+   // Prints each character in the given string with the given message.
    static void Print (string message, string str) {
-      WriteLine (message);
+      WriteLine ();
+      Write (message);
       foreach (char i in str) Write ($"{i} ");
+   }
+
+   // Getting the decimal value from the user for the given prompt.
+   static void GetResponse (string prompt, out double f) {
+      Write (prompt);
+      while (!double.TryParse (ReadLine (), out f)) {
+         ForegroundColor = ConsoleColor.Red;
+         WriteLine ("\tPlease enter any decimal value!");
+         ResetColor ();
+         Write (prompt);
+      }
    }
 }
