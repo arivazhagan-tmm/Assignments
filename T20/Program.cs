7462367ff0dd74a8634d0b8c6624312a6ed70493
Arivazhagan P <arivazhagan.p@metamation.com>
2023-10-25 17:51:39 +0530

    T20 : Review fix for split logic, method ordering, colon spacing.

--- T20/Program.cs
+++ T20/Program.cs
@@ -5,17 +5,22 @@ class Program {
    // Prints the integral & factorial part of the decimal number given by the user.
    static void Main () {
       while (true) {
-         GetResponse ("Enter a decimal number : ", out double f);
-         var txt = f.ToString ();
-         if (txt.Contains ('.')) {
-            var split = txt.Split ('.');
-            var (integral, factorial) = (split[0].Replace (".", ""), split[1]);
-            Print ("Integral Part\t: ", integral);
-            Print ("Factorial Part\t: ", factorial);
-         } else Print ("Integral Part\t: ", txt);
+         var prompt = "Enter a decimal number: ";
+         double f;
+         Write (prompt);
+         while (!double.TryParse (ReadLine (), out f)) {
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine ("\tPlease enter any decimal value!");
+            ResetColor ();
+            Write (prompt);
+         }
+         var split = f.ToString ().Split ('.');
+         Print ("Integral part\t: ", split[0]);
+         if (split.Length > 1)
+            Print ("Factorial part\t: ", split[1]);
          WriteLine ();
          WriteLine ();
-         Write ("Do you want to continue? (Y/N) : ");
+         Write ("Do you want to continue? (Y/N): ");
          if (ReadLine ()?.ToLower () is not "y") break;
          WriteLine ();
       }
@@ -28,14 +33,4 @@ class Program {
       foreach (char i in str) Write ($"{i} ");
    }
 
-   // Getting the decimal value from the user for the given prompt.
-   static void GetResponse (string prompt, out double f) {
-      Write (prompt);
-      while (!double.TryParse (ReadLine (), out f)) {
-         ForegroundColor = ConsoleColor.Red;
-         WriteLine ("\tPlease enter any decimal value!");
-         ResetColor ();
-         Write (prompt);
-      }
-   }
 }
