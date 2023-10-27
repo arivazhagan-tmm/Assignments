37fd7e8c55fd1f01ae46f28c271334a6ecd0b9ee
Arivazhagan P <arivazhagan.p@metamation.com>
2023-10-27 10:47:46 +0530

    T07 : Review fix for modifying switch expression.

--- T07/Program.cs
+++ T07/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using static System.Console;
+using static System.String;
 
 OutputEncoding = Encoding.Unicode;
 WriteLine ("┌───┬───┬───┬───┬───┬───┬───┬───┐");
@@ -11,17 +12,17 @@ var connector = "├───┼───┼───┼───┼───┼
 var verticalBar = "│";
 for (int i = 0, size = 8, padLength = 4; i < size; i++) {
    Write (verticalBar);
-   Action todo = i switch {
-      0 => () => Write (blackCoins),
-      1 => () => Write (blackPawns),
-      6 => () => Write (whitePawns),
-      7 => () => Write (whiteCoins),
-      _ => () => { for (int j = 0; j < size; j++) Write (verticalBar.PadLeft (padLength)); }
+   var s = i switch {
+      0 => blackCoins,
+      1 => blackPawns,
+      6 => whitePawns,
+      7 => whiteCoins,
+      < 6 => Concat (Enumerable.Repeat (verticalBar.PadLeft (padLength), size)),
+      _ => Empty
    };
-   todo ();
-   if (i == size - 1) break;
+   Write (s);
    WriteLine ();
+   if (i == size - 1) break;
    WriteLine (connector);
 }
-WriteLine ();
 WriteLine ("└───┴───┴───┴───┴───┴───┴───┴───┘");
