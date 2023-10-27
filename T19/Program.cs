8e5c24a905c23ce018605f172cffe9299cdf7662
Arivazhagan P <arivazhagan.p@metamation.com>
2023-10-27 15:44:20 +0530

    Added methods and comments

--- T19/Program.cs
+++ T19/Program.cs
@@ -1,13 +1,31 @@
 ﻿using static System.Console;
+
 class Program {
-   static void Main (string[] args) {
+   // Prints the factorial of the user given number.
+   static void Main () {
       while (true) {
-         Write ("Enter the number : ");
-         if (int.TryParse (ReadLine (), out int n)) {
-            long fact = 1;
-            do fact *= n * --n; while ((--n - 1) > 0);
-            Write ($"Factorial : {fact}\n");
-         } else break;
+         var prompt = "Enter the number: ";
+         int n;
+         Write (prompt);
+         while (!int.TryParse (ReadLine (), out n) || n > 25) {
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine ("\tPlease enter any whole number between 0 and 25.");
+            ResetColor ();
+            Write (prompt);
+         }
+         Write ($"Factorial of {n}: {GetFactorial (n)}");
+         WriteLine ();
+         WriteLine ();
+         Write ("Do you want to continue? (Y/N): ");
+         if (ReadLine ()?.ToLower () is not "y") break;
+         WriteLine ();
       }
+   }
+
+   // Computes and returns the factorial of given number.
+   static long GetFactorial (int n) {
+      long fact = 1;
+      do fact *= n * --n; while ((--n - 1) > 0);
+      return fact;
    }
 }
