3038d028404a515aba88c1b5edddecda25e460f2
Arivazhagan P <arivazhagan.p@metamation.com>
2023-10-27 15:16:37 +0530

    Minor changes

--- T18.1/Program.cs
+++ T18.1/Program.cs
@@ -1,28 +1,22 @@
-﻿class Program {
+﻿using static System.Console;
+
+class Program {
+   // Prints a number from the consecutive armstrong numbers at the user given index.
+   // User is allowed to enter the index through the command prompt.
    static void Main (string[] args) {
       if (int.TryParse (args[0], out var index) && index <= 25) {
          var (i, count, tmp) = (1, 0, 0);
-         for (; i < int.MaxValue; i++) {
-            if (IsArmstrong (i)) {
-               tmp = i;
-               count++;
-            }
-            if (count == index) {
-               Console.WriteLine ($"{tmp}");
-               return;
-            }
-         }
-      }
-      else
-            Console.WriteLine("Please enter index between 1 and 25");
-    }
+         while (count != index)
+            (tmp, count) = IsArmstrong (i++) ? (i - 1, count + 1) : (tmp, count);
+         WriteLine ($"Armstrong number at index {index} is {tmp}.");
+      } else WriteLine ("Please enter index between 1 and 25.");
+   }
 
-   static bool IsArmstrong (int n) {
-      double result = 0;
-      var chars = n.ToString ();
-      var len = chars.Length;
-      foreach (char ch in chars)
-         result += Math.Pow (double.Parse (ch.ToString ()), len);
-      return result == n;
+   // Checks whether the given number is armstrong or not.
+   static bool IsArmstrong (int number) {
+      var (str, res) = (number.ToString (), 0.0);
+      var len = str.Length;
+      foreach (var ch in str) res += Math.Pow (double.Parse (ch.ToString ()), len);
+      return res == number;
    }
 }
