3963723f3faf205eaedbe615e7932fd1fc529e96
Arivazhagan P <arivazhagan.p@metamation.com>
2023-10-27 12:12:25 +0530

    T23 - Voting contest

---
+++ T23/Program.cs
@@ -0,0 +1,46 @@
+﻿using static System.Console;
+
+Write ("\tSample test case");
+WriteLine ();
+var polls = "AabBBcd";
+var (winner, vote) = GetWinner (polls);
+PrintResult (polls, winner, vote);
+
+while (true) {
+   WriteLine ();
+   var prompt = "Poll your vote to find the winner: ";
+   bool validPoll;
+   do {
+      Write (prompt);
+      polls = ReadLine () ?? "";
+      validPoll = polls.Length != 0 && polls.All (char.IsLetter);
+      if (!validPoll) {
+         ForegroundColor = ConsoleColor.Red;
+         Write ("\tVotes are alphabets and minimum 2 votes to be polled.");
+         WriteLine ();
+         ResetColor ();
+      }
+   } while (!validPoll);
+   (winner, vote) = GetWinner (polls);
+   PrintResult (polls, winner, vote);
+   Write ("Do you want to continue? (Y/N): ");
+   if (ReadLine ()?.ToLower () is not "y") break;
+   WriteLine ();
+}
+
+// Prints the polled votes, winner and max vote.
+static void PrintResult (string polls, char winner, int vote) {
+   WriteLine ();
+   WriteLine ($"\tPolls\t: {polls}\n\tWinner\t: {char.ToUpper (winner)}\n\tVotes\t: {vote}");
+   WriteLine ();
+}
+
+// Finds and returns the winner along with their votes.
+static (char, int) GetWinner (string polls) {
+   var freqPairs = new Dictionary<char, int> ();
+   foreach (char ch in polls.ToLower ())
+      freqPairs[ch] = freqPairs.TryGetValue (ch, out int value) ? ++value : 1;
+   var frequencies = freqPairs.Values.ToList ();
+   var winner = freqPairs.ElementAt (frequencies.IndexOf (frequencies.Max ()));
+   return (winner.Key, winner.Value);
+}
