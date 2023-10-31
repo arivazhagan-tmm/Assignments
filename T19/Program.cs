3964181b6cc30e7121fe23315bfb15ad2b3b1e66
Arivazhagan P <arivazhagan.p@metamation.com>
2023-10-31 12:26:47 +0530

    Added new line characters

--- T19/Program.cs
+++ T19/Program.cs
@@ -7,22 +7,20 @@ class Program {
          var prompt = "Enter the number: ";
          int n;
          Write (prompt);
-         while (!int.TryParse (ReadLine (), out n) || n > 25) {
+         while (!int.TryParse (ReadLine (), out n) || n > 10) {
             ForegroundColor = ConsoleColor.Red;
-            WriteLine ("\tPlease enter any whole number between 0 and 25.");
+            WriteLine ("\tPlease enter any whole number between 0 and 10.");
             ResetColor ();
             Write (prompt);
          }
-         Write ($"Factorial of {n}: {GetFactorial (n)}");
-         WriteLine ();
-         WriteLine ();
-         Write ("Do you want to continue? (Y/N): ");
-         if (ReadLine ()?.ToLower () is not "y") break;
-         WriteLine ();
+         Write ($"Factorial of {n}: {GetFactorial (n)}\n\n");
+         Write ("Do you want to continue? (y/n): ");
+         if (ReadKey ().Key is not ConsoleKey.Y) break;
+         WriteLine ("\n");
       }
    }
 
-   // Computes and returns the factorial of given number.
+   // Computes and returns the factorial of the given number.
    static long GetFactorial (int n) {
       long fact = 1;
       do fact *= n * --n; while ((--n - 1) > 0);
