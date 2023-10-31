b00f39d6e43e3ae3536d1ebaabaf3a84f548497f
Arivazhagan P <arivazhagan.p@metamation.com>
2023-10-31 12:46:19 +0530

    Added new line characters.

--- T24/Program.cs
+++ T24/Program.cs
@@ -5,15 +5,13 @@ while (true) {
    GetResponse ("Enter the price\t: ", 0, out int price);
    GetResponse ("Enter number of exchange wrappers: ", 1, out int exchange);
    Compute (money, price, ref exchange, out int chocolates);
-   WriteLine ();
-   Write ($"\tNo of chocolates: {chocolates}\n\tRemaining money : {money % price}\n\tRemaining wrappers : {exchange}\n");
-   WriteLine ();
-   Write ("Do you want to continue? (Y/N): ");
-   if (ReadLine ()?.ToLower () is not "y") break;
-   WriteLine ();
+   Write ($"\n\tNo of chocolates:\t{chocolates}\n\tRemaining money:\t{money % price}\n\tRemaining wrappers:\t{exchange}\n\n");
+   Write ("Do you want to continue? (y/n): ");
+   if (ReadKey ().Key is not ConsoleKey.Y) break;
+   WriteLine ("\n");
 }
 
-// Computes number of chocolates and the remaining wrappers for the given money
+// Computes number of chocolates and the remaining wrappers for the given money.
 static void Compute (int money, int price, ref int exchange, out int chocos) {
    chocos = money / price; // Number of chocolates on the first buy.
    int wraps = chocos; // Wrappers from bought chocolates.
@@ -26,18 +24,17 @@ static void Compute (int money, int price, ref int exchange, out int chocos) {
    exchange = wraps; // Remaining wrappers which can't be exchanged.
 }
 
-// Getting valid response from the user for the given prompt.
+// Getting the valid response from the user for the given prompt.
 static void GetResponse (string prompt, int limit, out int number) {
-   string? response = "";
-   number = 0;
+   bool isValid;
    do {
-      if (response != "" && number <= limit) {
+      Write (prompt);
+      isValid = int.TryParse (ReadLine (), out number) && number > limit;
+      if (!isValid) {
          ForegroundColor = ConsoleColor.Red;
          WriteLine ($"\tThe number should be greater than {limit}.");
          ResetColor ();
       }
-      Write (prompt);
-      response = ReadLine ();
-   } while (!int.TryParse (response, out number) || number <= limit);
+   } while (!isValid);
 }
 
