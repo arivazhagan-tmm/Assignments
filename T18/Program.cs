1a989c5a59877827db7ca1089638c4037c37be30
Arivazhagan P <arivazhagan.p@metamation.com>
2023-10-31 12:02:21 +0530

    Spelling mistake corrections

--- T18/Program.cs
+++ T18/Program.cs
@@ -2,12 +2,11 @@
 
 while (true) {
    GetResponse ("Enter the number: ", out int n);
-   var result = IsArmstrong (n) ? "Armstrong" : "Not Armstrong";
-   WriteLine ($"{n} is {result} number.");
-   WriteLine ();
-   Write ("Do you want to continue? (Y/N): ");
-   if (ReadLine ()?.ToLower () is not "y") break;
-   WriteLine ();
+   var res = IsArmstrong (n) ? "armstrong" : "not armstrong";
+   WriteLine ($"{n} is {res} number.\n");
+   Write ("Do you want to continue? (y/n): ");
+   if (ReadKey ().Key is not ConsoleKey.Y) break;
+   WriteLine ("\n");
 }
 
 // Getting the valid numeric response from the user for the given prompt.
@@ -15,17 +14,17 @@ static void GetResponse (string prompt, out int n) {
    Write (prompt);
    while (!int.TryParse (ReadLine (), out n) || n <= 0) {
       ForegroundColor = ConsoleColor.Red;
-      WriteLine ("\tPlease enter any whole number which is greater than 0.");
+      WriteLine ("\tPlease enter any whole number that is greater than 0");
       ResetColor ();
       Write (prompt);
    }
 }
 
 // Checks whether the given number is armstrong or not.
-static bool IsArmstrong (int number) {
-   var (str, res) = (number.ToString (), 0.0);
+static bool IsArmstrong (int n) {
+   var (str, res) = (n.ToString (), 0.0);
    var len = str.Length;
    foreach (var ch in str) res += Math.Pow (double.Parse (ch.ToString ()), len);
-   return res == number;
+   return res == n;
 }
 
