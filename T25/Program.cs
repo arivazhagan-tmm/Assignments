bd530cb0765e777a4b42040e014a3e285677f2f6
arivazhagan <arivazhagan.p@metamation.com>
2023-11-08 14:34:29 +0530

    T25: Minor logic changes.

--- T25/Program.cs
+++ T25/Program.cs
@@ -4,11 +4,12 @@ using static Order;
 while (true) {
    GetString ("Enter the letters: ", out var letters);
    GetChar ("Enter the reference letter: ", out var refChar);
+   WriteLine ();
    GetChar ("Type \'A\' for ascending order or \'D\' for descending order: ", out var ch, true);
    var chars = letters.ToCharArray ();
    var order = ch is 'd' or 'D' ? Descending : Ascending;
    Sort (ref chars, refChar, order);
-   Write ($"\n\tLetters:{letters}\n\tSorted:");
+   Write ($"\nLetters:{letters}\nSorted:");
    foreach (char c in chars) Write (c);
    Write ("\n\nDo you want to continue? (y/n): ");
    if (ReadKey ().Key is not ConsoleKey.Y) break;
@@ -17,37 +18,33 @@ while (true) {
 
 // Getting the valid character from the user for the given prompt.
 static void GetChar (string prompt, out char ch, bool askingOrder = false) {
-   bool isValid;
    var msg = askingOrder ? "either A or D" : "an alphabet";
-   do {
-      Write (prompt);
+   Write (prompt);
+   while (true) {
       ch = (char)ReadKey ().Key;
-      isValid = askingOrder ? ch is 'A' or 'a' or 'd' or 'D' : char.IsLetter (ch);
-      if (!isValid) {
-         ForegroundColor = ConsoleColor.Red;
-         WriteLine ($"\tInput should be {msg}.");
-         ResetColor ();
-      }
-      WriteLine ();
-   } while (!isValid);
+      bool isValid = askingOrder ? ch is 'A' or 'a' or 'd' or 'D' : char.IsLetter (ch);
+      if (isValid) break;
+      ForegroundColor = ConsoleColor.Red;
+      WriteLine ($"\nInput should be {msg}.");
+      ResetColor ();
+      Write (prompt);
+   }
 }
 
 // Getting the valid string from the user for the given prompt.
 static void GetString (string prompt, out string response) {
-   bool isValid;
-   do {
-      Write (prompt);
+   Write (prompt);
+   while (true) {
       response = ReadLine () ?? "";
-      isValid = response.All (char.IsLetter);
-      if (!isValid) {
-         ForegroundColor = ConsoleColor.Red;
-         WriteLine ("\tInput should contain only alphabets.");
-         ResetColor ();
-      }
-   } while (!isValid);
+      if (response.All (char.IsLetter)) break;
+      ForegroundColor = ConsoleColor.Red;
+      WriteLine ("Input should contain only alphabets.\n");
+      ResetColor ();
+      Write (prompt);
+   }
 }
 
-// Sorts the given character array for the given order.
+// Sorts the given character array in the given order.
 static void Sort (ref char[] chars, char refChar, Order order = Ascending) {
    var (i, len, list) = (0, chars.Length, chars.ToList ());
    var (ptr, increment) = order == Ascending ? (0, 1) : (len - 1, -1);
