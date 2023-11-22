dba46514280b09b5699e0f9737e2cc18e133a0ae
arivazhagan <arivazhagan.p@metamation.com>
2023-11-22 12:28:27 +0530

    T26: MyList<T>

---
+++ T26/MyList.cs
@@ -0,0 +1,115 @@
+﻿namespace T26 {
+   public class MyList<T> {
+      #region Constructor
+      public MyList () {
+         (mCount, mCapacity) = (0, 4);
+         mElements = new T[mCapacity];
+         mDefault = default;
+      }
+      #endregion
+
+      #region Public Properties
+      public int Count { get => mCount; }
+      public int Capacity { get => mCapacity; }
+      public T this[int index]
+      {
+         get
+         {
+            ValidateIndex (index);
+            return mElements[index];
+         }
+         set
+         {
+            ValidateIndex (index);
+            mElements[index] = value;
+         }
+      }
+      #endregion
+
+      #region Public Methods
+      /// <summary> Adds given element into the list. </summary>
+      // Doubles the list capacity when it is completly filled.
+      public void Add (T a) {
+         Update ();
+         mElements[mCount++] = a;
+      }
+
+      /// <summary> Clears all the elements from list by setting default values to them. </summary>
+      public void Clear () {
+         if (mDefault != null)
+            for (int i = 0; i < mCount;) mElements[i++] = mDefault;
+         mCount = 0;
+      }
+
+      /// <summary> Inserts the given element at the given index. </summary>
+      // Throws an exception if the given index is not in the valid range.
+      // Doubles the list capacity if it is filled to current capacity.
+      public void Insert (int index, T a) {
+         ValidateArgument (index);
+         Update ();
+         var tmp = new T[mCapacity];
+         for (int i = 0, j = 0; i < mCount; i++) {
+            if (i == index) (tmp[i], j) = (a, 1);
+            tmp[i + j] = mElements[i];
+         }
+         mElements = tmp;
+         mCount++;
+      }
+
+      /// <summary> Removes given element from list. </summary>
+      // Throws an exception if the given element is not present in the list.
+      public bool Remove (T a) {
+         for (int i = 0; i < mCount; i++) {
+            T tmp = mElements[i];
+            if (tmp != null && tmp.Equals (a)) {
+               RemoveAt (i);
+               return true;
+            }
+         }
+         return false;
+      }
+
+      /// <summary> Removes the element present in given index. </summary>
+      // Throws an exception if the given index is not in the valid range.
+      public void RemoveAt (int index) {
+         ValidateArgument (index);
+         bool found = false;
+         for (int i = 0; i < mCount; i++) {
+            if (i == index) found = true;
+            if (found && i + 1 < mCount) mElements[i] = mElements[i + 1];
+         }
+         if (mDefault != null) mElements[--mCount] = mDefault;
+      }
+      #endregion
+
+      #region Private Methods
+      bool IsInvalid (int val) => val < 0 || val >= mCount;
+
+      void ValidateArgument (int arg) {
+         if (IsInvalid (arg)) throw new ArgumentOutOfRangeException ($"Given argument {arg} is not in the valid range.");
+      }
+
+      /// <summary> Validates the given index for non negativity and max limit condition. </summary>
+      void ValidateIndex (int index) {
+         if (IsInvalid (index)) throw new IndexOutOfRangeException ($"Cannot access the element at the given index {index}.");
+      }
+
+      /// <summary> Doubles the current list capacity. </summary>
+      void Update () {
+         if (mCount == mCapacity) {
+            mCapacity *= 2;
+            var tmp = new T[mCapacity];
+            for (int i = 0; i < mCount; i++) tmp[i] = mElements[i];
+            mElements = tmp;
+         }
+      }
+      #endregion
+
+      #region Private Fields
+      T[] mElements;
+      readonly T? mDefault;
+      int mCount;
+      int mCapacity;
+      #endregion
+   }
+}
---
+++ T26/Program.cs
@@ -0,0 +1,37 @@
+﻿using T26;
+using static System.Console;
+
+var mList = new MyList<int> ();
+Random r = new ();
+for (int i = 1; i <= 50; i++) {
+   var (tmp1, tmp2) = (r.NextDouble (), r.Next (0, i));
+   if (tmp1 <= 0.25) {
+      mList.Add (i);
+      ForegroundColor = ConsoleColor.Green;
+      CursorLeft = (WindowWidth / 5) - 5;
+      WriteLine ($"\nAdded : {i}");
+      ShowList ();
+   } else if (tmp1 is > 0.25 and <= 0.5 && tmp2 < mList.Count) {
+      mList.RemoveAt (tmp2);
+      ForegroundColor = ConsoleColor.Red;
+      WriteLine ($"\nRemoved at : {tmp2}");
+      ShowList ();
+   } else if (tmp2 > 0 && tmp2 < mList.Count) {
+      tmp2 -= 1;
+      mList.Insert (tmp2, i);
+      ForegroundColor = ConsoleColor.Yellow;
+      WriteLine ($"\nInserted {i} at : {tmp2}");
+      ShowList ();
+   }
+}
+ForegroundColor = ConsoleColor.White;
+WriteLine ($"\nList count is {mList.Count} and capacity is {mList.Capacity}");
+
+void ShowList () {
+   ResetColor ();
+   if (mList?.Count > 0) {
+      Write ("List items : ");
+      for (int j = 0, count = mList.Count; j < count; j++) Write ($" {mList[j]},");
+      WriteLine ();
+   } else Write ("\nList is Empty!!");
+}
