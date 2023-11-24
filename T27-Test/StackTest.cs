f02ddc67470f8ca9c378978b20fc33dd79bc26c3
arivazhagan <arivazhagan.p@metamation.com>
2023-11-24 15:45:44 +0530

    T27: Review fix for removing unwanted lines, adding region, writing summary for properties.

---
+++ T27-Test/StackTest.cs
@@ -0,0 +1,64 @@
+using T27;
+using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace T27_Test {
+   #region Public Class-----------------------------------------------------------------------------
+   /// <summary>A test class which performs sequence of tests for each functionalities of class "TStack"</summary>
+   [TestClass]
+   public class StackTest {
+      #region Public Methods------------------------------------------
+      /// <summary> Initiates the testing for stack functionalities.</summary>
+      [TestMethod]
+      public void InitiateTest () {
+         TestPush ();
+         TestPop ();
+         TestPeek ();
+         TestExceptions ();
+      }
+      #endregion
+
+      #region Private Methods-----------------------------------------
+      /// <summary> Tests the current count elements in the stack using equality assertion.</summary>
+      void TestCount () => AreEqual (mTStack.Count, mStack.Count);
+
+      /// <summary> Tests the arise of exceptions when invalid operation is performed.</summary>
+      void TestExceptions () {
+         mStack.Clear ();
+         mTStack.Clear ();
+         Action mStackPeek = () => mStack.Peek (),
+                mStackPop = () => mStack.Pop (),
+                mTStackPeek = () => mTStack.Peek (),
+                mTStackPop = () => mTStack.Pop ();
+         ThrowsException<InvalidOperationException> (mStackPeek);
+         ThrowsException<InvalidOperationException> (mStackPop);
+         ThrowsException<InvalidOperationException> (mTStackPeek);
+         ThrowsException<InvalidOperationException> (mTStackPop);
+      }
+
+      /// <summary> Tests the peek functionality using equality assertion.</summary>
+      void TestPeek () => AreEqual (mTStack.Peek (), mStack.Peek ());
+
+      /// <summary> Tests the pop functionality using equality assertion.</summary>
+      void TestPop () => AreEqual (mTStack.Pop (), mStack.Pop ());
+
+      /// <summary> Tests the push functionality using equality assertion.</summary>
+      // Tests the count of the elements in the stack and TStack.
+      // Tests the equality of recently pushed elements.
+      void TestPush () {
+         int[] ints = { 1, 2, 3, 4, 5, };
+         foreach (var i in ints) {
+            mStack.Push (i);
+            mTStack.Push (i);
+            TestCount ();
+            TestPeek ();
+         }
+      }
+      #endregion
+
+      #region Private Fields------------------------------------------
+      Stack<int> mStack = new ();
+      TStack<int> mTStack = new ();
+      #endregion
+   }
+   #endregion
+}
--- T27/TStack.cs
+++ T27/TStack.cs
@@ -1,21 +1,25 @@
 ﻿namespace T27;
 
+#region Public Class--------------------------------------------------------------------------------
+/// <summary> A generic class depicts the typical functionality of the conventional class "Stack".</summary>
 public class TStack<T> {
 
-   #region Public Properties
+   #region Public Properties------------------------------------------
+   /// <summary>State of stack either empty or filled.</summary>
    public bool IsEmpty => mPointer is 0;
+   /// <summary> Current capacity of the stack to store elements.</summary>
    public int Length => mElements.Length;
-   public int Count { get => mPointer; }
+   /// <summary> Number of elements currently present in the stack.</summary>
+   public int Count => mPointer;
    #endregion
 
-   #region Public Methods
+   #region Public Methods---------------------------------------------
    /// <summary> Pushes the given element into the stack.</summary>
    // Doubles the stack capacity when the stack is completely filled to the current capacity.
    public void Push (T a) {
       if (mPointer == Length) {
          var tmp = new T[Length * 2];
-         for (int i = 0; i < Length; i++)
-            tmp[i] = mElements[i];
+         for (int i = 0; i < Length; i++) tmp[i] = mElements[i];
          mElements = tmp;
       }
       mElements[mPointer++] = a;
@@ -37,13 +41,14 @@ public class TStack<T> {
 
    /// <summary> Clears the stack and sets properties to defaults. </summary>
    public void Clear () {
-      mElements = new T[Length];
+      Array.Clear (mElements);
       mPointer = 0;
    }
    #endregion
 
-   #region Private Data Members
+   #region Private Fields---------------------------------------------
    T[] mElements = new T[4];
    int mPointer;
    #endregion
 }
+#endregion
