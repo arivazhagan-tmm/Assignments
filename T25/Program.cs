049e17654a593464b4e1e4307aeb54b8cc49beac
arivazhagan <arivazhagan.p@metamation.com>
2023-11-29 18:01:10 +0530

    T25: Review changes for sort logic and removing unwanted variables

--- T25/Program.cs
+++ T25/Program.cs
@@ -7,7 +7,7 @@ while (true) {
    WriteLine ();
    GetChar ("Type \'A\' for ascending order or \'D\' for descending order: ", out var ch, true);
    var chars = letters.ToCharArray ();
-   var order = char.ToUpper (ch) is 'D' ? Descending : Ascending;
+   var order = ch is 'D' ? Descending : Ascending;
    chars = Sort (chars, splChar, order);
    Write ($"\nLetters:{letters}\nSorted:");
    foreach (char c in chars) Write (c);
@@ -16,23 +16,21 @@ while (true) {
    WriteLine ("\n");
 }
 
-// Getting the valid character from the user for the given prompt.
+//Getting the valid character from the user for the given prompt.
 static void GetChar (string prompt, out char ch, bool askingOrder = false) {
    Write (prompt);
-   var (msg, msgUpdated) = ("", false);
    while (true) {
-      ch = (char)ReadKey ().Key;
-      bool isValid = askingOrder ? ch is 'A' or 'a' or 'd' or 'D' : char.IsLetter (ch);
+      ch = char.ToUpper ((char)ReadKey ().Key);
+      bool isValid = askingOrder ? ch is 'A' or 'D' : char.IsLetter (ch);
       if (isValid) break;
       ForegroundColor = ConsoleColor.Red;
-      if (!msgUpdated) msg = askingOrder ? "either A or D" : "an alphabet";
-      WriteLine ($"\nInput should be {msg}.");
+      WriteLine ($"\nInput should be {(askingOrder ? "either A or D" : "an alphabet")}.");
       ResetColor ();
       Write (prompt);
    }
 }
 
-// Getting the valid string from the user for the given prompt.
+//Getting the valid string from the user for the given prompt.
 static void GetString (string prompt, out string response) {
    Write (prompt);
    while (true) {
@@ -45,21 +43,12 @@ static void GetString (string prompt, out string response) {
    }
 }
 
-// Sorts the given character array in the given order.
+//Sorts and returns the given character array in the given order.
 static char[] Sort (char[] chars, char splChar, Order order = Ascending) {
    splChar = char.ToLower (splChar);
-   var (len, list) = (chars.Length, chars.ToList ());
-   list.RemoveAll (x => x == splChar);
-   var splCharCount = len - list.Count;
-   var (ptr, increment) = order == Ascending ? (0, 1) : (len - (splCharCount + 1), -1);
-   var res = new char[len];
-   while (list.Count > 0) {
-      res[ptr] = list.Min ();
-      list.Remove (res[ptr]);
-      ptr += increment;
-      if (splCharCount > 0) res[len - splCharCount--] = splChar;
-   }
-   return res;
+   char[] a = chars.Where (c => c != splChar).ToArray ();
+   var sortedArr = order == Ascending ? a.Order () : a.OrderDescending ();
+   return sortedArr.Concat (chars.Where (c => c == splChar)).ToArray ();
 }
 
 public enum Order { Ascending, Descending }
